4ed2715ee97d474f94acc15a12236346f3a6c086
Logan Fetterly <lfetterly25@gmail.com>
2022-09-19 20:08:26 -0600

    Vercel Url env parameter added

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,7 +1,7 @@
 import { Experience } from "../typings";
 
 export const fetchExperiences = async() => {
-    const res = await fetch(`http://${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+    const res = await fetch(`http://${process.env.VERCEL_URL}/api/getExperience`);
 
     const data = await res.json()
     const experiences: Experience[] = data.experiences;

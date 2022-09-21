48f56573499162131c703a2dc7f9a3ed5a4f625c
Logan Fetterly <lfetterly25@gmail.com>
2022-09-20 23:05:43 -0600

    trying NEXT_PUBLIC_URL

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,7 +1,7 @@
 import { Social } from "../typings";
 
 export const fetchSocials = async() => {
-    const res = await fetch(`http://${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`,
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getSocials`,
     {
       method: "GET",
       headers: {
